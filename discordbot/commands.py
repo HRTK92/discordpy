c2b868c35496f62cccdd3ffa120ea0ba20c02acc
HRTK92 <takuho0920@outlook.jp>
2021-01-01 11:17:40 +0000

    update

--- discordbot/commands.py
+++ discordbot/commands.py
@@ -2,6 +2,8 @@ import requests
 import discord
 from discord.ext import commands
 import time
+from gtts import gTTS
+
 async def is_owner(ctx):
     return ctx.author.id == 618332297275375636
 class Commands(commands.Cog, name='コマンド'):
@@ -19,3 +21,12 @@ class Commands(commands.Cog, name='コマンド'):
 	  embed=discord.Embed(title="使い方", description="```.fn <ユーザー名>```  Fortniteの成績を表示します", color=0x4273b5)
 	  embed.set_footer(text=".helpでBOTのヘルプを表示")
 	  await ctx.send(embed=embed)
+	  
+	@commands.command()
+	@commands.check(is_owner)
+	async def speak(self, ctx,*args):
+	  tts_ja = gTTS(text=args[0], lang='ja', slow=False)
+	  tts_ja.save("text.mp3")
+	  ctx.guild.voice_client.play(tts_ja)
+    
+
