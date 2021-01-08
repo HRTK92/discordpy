05aeec9a097d5d56552a267dc0377e259be575f8
HRTK92 <takuho0920@outlook.jp>
2021-01-08 10:07:36 +0000

    upload

---
+++ modules/commands_apex.py
@@ -0,0 +1,11 @@
+class Commands_fn(commands.Cog, name='Apex β版'):
+    def __init__(self, bot):
+        self.bot = bot
+
+        @commands.command()
+        async def map(self, ctx):
+            """Apex"""
+            text = "Fortnite map"
+            embed = discord.Embed(title=text, color=0x00ff00)
+            embed.set_image(url="https://media.fortniteapi.io/images/map.png")
+            await ctx.send(embed=embed)
