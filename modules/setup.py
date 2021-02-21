31d865d0ee5d506ca541db03133ec7c4e1271c2a
HRTK92 <70054655+HRTK92@users.noreply.github.com>
2021-02-21 09:12:12 +0900

    Update and rename index.py to modules/setup.py

---
+++ modules/setup.py
@@ -0,0 +1,20 @@
+import asyncio
+import json
+import platform
+import discord
+
+import modules
+
+def start(token):
+  client = modules.Mybot()
+  client.add_cog(modules.Commands(client))
+  client.add_cog(modules.Commands_fn(client))
+  client.add_cog(modules.Commands_ch(client))
+  client.add_cog(modules.Music(client))
+  client.add_cog(modules.Poll(client))
+  client.add_cog(modules.Commands_test(client))
+  print(f'python {platform.python_version()}')
+  print("discord.py " + discord.__version__)
+  loop = asyncio.get_event_loop()
+  #loop.run_until_complete(modules.setup())
+  loop.run_until_complete(client.run(token))
